b1a099ae3cd19572abc8a3c4cab22ee9b7b419d3
Thomas Hopf <thomas.hopf@gmail.com>
2025-04-23 19:09:32 +0200

    transform single mutation scan into instances

--- src/pages/results/results.tsx
+++ src/pages/results/results.tsx
@@ -18,7 +18,12 @@ import {
   SingleMutationScanApiResult,
 } from "../../models/api.ts";
 import { DNAGenerationDialog } from "./dna.tsx";
-import { SystemInstanceSpec } from "../../models/design.ts";
+import {
+  EntitySpec,
+  SingleMutationScanResult,
+  SystemInstanceSpec,
+} from "../../models/design.ts";
+import { useMemo } from "react";
 
 export interface FinishedResultsWrapperProps {
   id: string;
@@ -27,6 +32,55 @@ export interface FinishedResultsWrapperProps {
     | SingleMutationScanApiResult
     | ProteinToDnaApiResult;
 }
+
+const singleMutationScanToInstances = (
+  system: EntitySpec[],
+  systemInstance: SystemInstanceSpec,
+  scores: SingleMutationScanResult[],
+): SystemInstanceSpec[] => {
+  const instances: SystemInstanceSpec[] = [];
+
+  scores.forEach((row: SingleMutationScanResult) => {
+    row.subs.forEach((mut) => {
+      // skip deletion for now
+      if (mut.to === "-") {
+        return;
+      }
+
+      // skip self substitution, otherwise would include once per position
+      if (mut.to === row.ref) {
+        return;
+      }
+
+      // make deep copy of instance to be safe
+      const curInstance: SystemInstanceSpec = JSON.parse(
+        JSON.stringify(systemInstance),
+      );
+
+      // mutate instance, add mutant as metadata
+      const rep = curInstance.entity_instances[row.entity].rep;
+      const mutIndex = row.pos - system[row.entity].first_index;
+
+      if (rep.charAt(mutIndex) !== row.ref) {
+        throw new Error(
+          "Invalid reference character, this should never happen",
+        );
+      }
+
+      curInstance.entity_instances[row.entity].rep =
+        rep.substring(0, mutIndex) + mut.to + rep.substring(mutIndex + 1);
+
+      curInstance.metadata = {
+        mutant: `${row.entity}:${row.ref}${row.pos}${mut.to}`,
+      };
+
+      // attach to instance list
+      instances.push(curInstance);
+    });
+  });
+
+  return instances;
+};
 
 export const FinishedResultsPageWrapper = ({
   id,
@@ -41,13 +95,21 @@ export const FinishedResultsPageWrapper = ({
     ? (results as PipelineApiResult | SingleMutationScanApiResult).spec.system
     : (results as ProteinToDnaApiResult).spec.args.system;
 
-  let instances: SystemInstanceSpec[] | null = null;
+  let instances: SystemInstanceSpec[] | null = useMemo(() => {
+    // gather instances if available
+    if (results.spec.key === "pipeline") {
+      instances = (results as PipelineApiResult).instances;
+    } else if (results.spec.key === "single_mutation_scan") {
+      return singleMutationScanToInstances(
+        system,
+        results.spec.system_instance,
+        (results as SingleMutationScanApiResult).scores,
+      );
+    }
 
-  // gather instances if available
-  // TODO: create instances from single mutants for single mutation scan
-  if (results.spec.key === "pipeline") {
-    instances = (results as PipelineApiResult).instances;
-  }
+    // otherwise no instances available
+    return null;
+  }, [results]);
 
   // TODO: eventually render based on type of results, separate page for DNA download...
   return (
