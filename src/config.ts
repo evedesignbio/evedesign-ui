df289f0706076323037533d2bec20b42e4ffad93
Thomas Hopf <thomas.hopf@gmail.com>
2025-07-31 13:31:40 +0200

    change to markslab account

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // TODO: better to get this from env vars eventually
-export const SUPABASE_URL = "https://xgabsdyosgtwoqygjucp.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_gBCv_cVjvcF84wcjp2AHPg_qmQOxdIv";
+export const SUPABASE_URL = "https://zpdgrkiphiyrtivhhyhn.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_B1Sfq6kkBT-kdepmx7rgsw_N27GMbtQ";
