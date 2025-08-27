75a505d21152a9ac2c6be5dbd2c240c7a2ca5d5a
Thomas Hopf <thomas.hopf@gmail.com>
2025-08-27 17:59:00 +0200

    use production api now it has taxonomy report feature

--- src/api/mmseqs.ts
+++ src/api/mmseqs.ts
@@ -9,7 +9,7 @@ import {
 } from "../utils/bio.ts";
 import { MsaResult } from "../models/api.ts";
 
-const mmseqsBaseUrl = (): string => "https://api-dev.colabfold.com/";
+const mmseqsBaseUrl = (): string => "https://api.colabfold.com/";
 
 export const useMmseqsSearch = (seq: string | null) => {
   return usePolling(
